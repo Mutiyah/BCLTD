bdc4db856e9c31798e54f8b655616e4d843a5642
Mutiyah Muritala <mutiyah.kem@gmail.com>
2020-12-04 19:33:48 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+//Get the button scroll button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}   //End of scroll button
+
+
+
+
+// Ajax for form submit
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+  
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+  
+  function success() {
+    form.reset();
+    status.innerHTML = "Thank you!!! We will contact you shortly.";
+    status.style.backgroundColor = "#0A4273";
+    status.style.color = "white";
+    status.style.width = "90%";
+    status.style.padding = "15px";
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+    status.style.backgroundColor = "#0A4273";
+    status.style.color = "white";
+    status.style.width = "90%";
+    status.style.padding = "15px";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
